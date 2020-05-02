f5429e1504c75c3f97bd429adc05feb35898d262
jhadheeraj1986 <47793209+jhadheeraj1986@users.noreply.github.com>
2020-05-02 21:37:54 +0530

    Solution

--- python/FirstBadVersion.py
+++ python/FirstBadVersion.py
@@ -1,1 +1,16 @@
-
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        low = 0
+        high = n
+        while(low < high):
+            mid = low + (high - low)/2
+            if isBadVersion(mid):
+                high = mid
+            else:
+                low = mid + 1
+        
+        return int(low)
+  
